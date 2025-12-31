855da0b0cdefba999c9f82f6dad359afaf049f55
Tan <letutan500@gmail.com>
2025-12-31 17:08:57 +0700

    Update profile

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { Roles } from 'src/roles/decorators/roles.decorator';
 import { RolesGuard } from 'src/roles/guards/roles.guard';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import type { CurrentUser as ICurrentUser } from 'src/common/interfaces/current-user.interface';
+import { UserProfileDto } from './dto/user-profile.dto';
 
 @Controller('users')
 @UseGuards(JwtAuthGuard, RolesGuard)
@@ -22,6 +23,12 @@ export class UsersController {
 
   @Get('profile')
   @ApiBearerAuth('access-token')
+  @ApiOperation({ summary: 'Create or update a role with permissions' })
+  @ApiResponse({
+    status: 201,
+    description: 'Role created or updated successfully',
+    type: UserProfileDto,
+  })
   @UseGuards(JwtAuthGuard)
   async getProfile(@CurrentUser() user: ICurrentUser) {
     if (!user.id) {
